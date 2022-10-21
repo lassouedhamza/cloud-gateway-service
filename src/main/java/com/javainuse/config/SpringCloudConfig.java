9403a6a3777758f050e9b1474b2f0bee4b17f2c7
lassouedhamza <43931046+lassouedhamza@users.noreply.github.com>
2022-10-21 20:35:28 +0100

    cloud

---
+++ src/main/java/com/javainuse/config/SpringCloudConfig.java
@@ -0,0 +1,22 @@
+package com.javainuse.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringCloudConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/maisonHotes/**")
+                        .uri("lb://MAISON-HOTE-SERVICE")
+                        .id("em"))
+
+              
+                .build();
+    }
+
+}
